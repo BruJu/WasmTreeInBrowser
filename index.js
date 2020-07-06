ef15984e087128b85d6d7d03438f89fafad72667
BruJu <bruyat.ju@live.fr>
2020-07-06 11:46:03 +0200

    Basic interface to choose which benchmark to run

--- index.js
+++ index.js
@@ -11,6 +11,45 @@ const n3 = require("n3");
 
 /* == ACTUAL CODE == */
 
+function populateSourceFiles() {
+    let select = $("#sourcefile");
+    select.empty();
+
+    const availableFiles = [
+        { "triples":    10000, "filename": "persondata_en_10k.ttl"},
+        { "triples":    20000, "filename": "persondata_en_20k.ttl"},
+        { "triples":    40000, "filename": "persondata_en_40k.ttl"},
+        { "triples":    80000, "filename": "persondata_en_80k.ttl"},
+        { "triples":   100000, "filename": "persondata_en_100k.ttl"},
+        { "triples":  1000000, "filename": "persondata_en_1M.ttl"},
+        { "triples": 10310106, "filename": "persondata_en.ttl"}
+    ];
+
+    $.each(availableFiles, function() {
+        select.append(
+            $('<option />')
+                .val(this.filename)
+                .text(this.filename + " (" + this.triples + " triples)")
+        );
+    });
+}
+
+populateSourceFiles();
+
+datasetKinds = {};
+function addDatasetKind(initializer, name) {
+    let select = $("#datasetkind");
+
+    datasetKinds[name] = initializer;
+
+    select.append($('<option></option>')
+        .attr('value', name)
+        .text(name));
+}
+
+addDatasetKind(() => new wasmtree.Dataset(), "Wasm Tree");
+addDatasetKind(() => new sophia_wasm.ArrayDataset(), "Array Dataset");
+
 // Returns a pattern in the form of an array of 4 terms
 // Aligned with sophia_rdf_wasm_benchmark (or whatever name I gave to my sophia_benchmark fork)
 function getPatternForRequest(requestNumber) {
@@ -84,6 +123,48 @@ function benchmark(datasetInstancier, datasetFilename, requestNumber, callback) 
     },'text');
 }
 
-benchmark(() => new wasmtree.Dataset(), "persondata_en_1M.ttl", 1, 
-    console.log
-);
+//benchmark(() => new wasmtree.Dataset(), "persondata_en_1M.ttl", 1, 
+//    console.log
+//);
+
+function startbenchmark() {
+    let file = $("#sourcefile option:selected").val();
+    let dataset = $("#datasetkind option:selected").val();
+    let queryNumber = $("input[name='query']:checked").val();
+
+    if (file == null || dataset == null || queryNumber == null) {
+        // TODO : error message
+        return;
+    }
+
+    let instancier = datasetKinds[dataset];
+    if (instancier == null) {
+        // TODO : error message
+        return;
+    }
+
+    $("#startbenchmark").attr("disabled", "true");
+
+    benchmark(instancier, file, parseInt(queryNumber),
+        results => {
+            function f(num) {
+                return num.toLocaleString(undefined, { maximumFractionDigits: 3, minimumFractionDigits: 0 });
+            }
+
+            let parsedResults = [];
+            parsedResults.push({ label: "Fill (ms)", value: f(results.fill) });
+            parsedResults.push({ label: "Match (ms)", value: f(results.cachedMatch) });
+            parsedResults.push({ label: "Count (ms)", value: f(results.forEachTime) });
+            parsedResults.push({ label: "Match + Count (ms)", value: f(results.cachedMatch + results.forEachTime) });
+            parsedResults.push({ label: "# Quads", value: results.forEachCount });
+
+            let template = $("#resulttable_template").text();
+            let content = Mustache.render(template, parsedResults);
+            $("#resulttable").html(content);
+
+            $("#startbenchmark").attr("disabled", false);
+        }
+    );
+}
+
+$("#startbenchmark").click(startbenchmark);
